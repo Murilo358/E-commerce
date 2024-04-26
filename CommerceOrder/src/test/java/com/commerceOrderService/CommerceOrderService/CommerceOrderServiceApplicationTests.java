39ab5731228f5f2503ae25d239bec8749b004e8a
Murilo358 <murilobarbosa358@gmail.com>
2024-04-26 00:27:00 -0300

    created: new order service

---
+++ CommerceOrder/src/test/java/com/commerceOrderService/CommerceOrderService/CommerceOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerceOrderService.CommerceOrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
