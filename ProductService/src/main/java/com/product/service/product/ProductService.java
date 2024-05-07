b9c4b7950171a2170c3f4096b8b4d9bf5b258900
Murilo358 <murilobarbosa358@gmail.com>
2024-05-07 00:23:35 -0300

    recreated deleted file

---
+++ ProductService/src/main/java/com/product/service/product/ProductService.java
@@ -0,0 +1,42 @@
+package com.product.service.product;
+
+
+import com.product.service.category.CategoryService;
+import com.product.service.product.dto.CreateProductDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+    public Product createProduct(CreateProductDTO createProductDTO){
+
+        categoryService.findById(createProductDTO.categoryId());
+
+        Product product = new Product();
+        product.setCategoryId(createProductDTO.categoryId());
+        product.setName(createProductDTO.name());
+        product.setCreatedAt(LocalDateTime.now());
+        product.setSellerId(createProductDTO.sellerId());
+        product.setDescription(createProductDTO.description());
+        product.setPrice(createProductDTO.price());
+        product.setInventoryCount(createProductDTO.inventoryCount());
+
+        return productRepository.save(product);
+
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
