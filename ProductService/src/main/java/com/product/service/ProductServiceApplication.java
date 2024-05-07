a3cc4445b8d7dc6464c24e54909fcd27790ea61d
Murilo358 <murilobarbosa358@gmail.com>
2024-05-07 00:25:20 -0300

    renamed application main class

--- ProductService/src/main/java/com/product/service/ProductServiceApplication.java
+++ ProductService/src/main/java/com/product/service/ProductServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class CommerceCatalogApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommerceCatalogApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
