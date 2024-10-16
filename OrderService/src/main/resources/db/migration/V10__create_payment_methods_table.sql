2e61e1aa636d032a163e6b26a6f9595641ee3db4
Murilo358 <murilobarbosa358@gmail.com>
2024-10-16 20:12:16 -0300

    created payment_methods table

---
+++ OrderService/src/main/resources/db/migration/V10__create_payment_methods_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS users_management.payment_methods (
+    id serial not null,
+    name text not null
+)
---
+++ ProductService/src/main/resources/db/migration/V10__create_payment_methods_table.sql
@@ -0,0 +1,4 @@
+CREATE IF NOT EXISTS TABLE users_management.payment_methods (
+    id serial not null,
+    name text not null
+);
