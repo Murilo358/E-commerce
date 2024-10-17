31e65574175485119fe25b08854e7f891db76833
Murilo358 <murilobarbosa358@gmail.com>
2024-10-17 19:55:10 -0300

    Created PaymentMethodView

---
+++ ProductService/src/main/java/com/product/service/query/paymentMethod/PaymentMethodView.java
@@ -0,0 +1,25 @@
+package com.product.service.query.paymentMethod;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="payment_methods", schema="users_management")
+public class PaymentMethodView {
+    @Id
+    private UUID id;
+    @Column
+    private String name;
+}
