cb21ee45ea52002a6ce94fb5edf92494e7d9b4ab
Murilo358 <murilobarbosa358@gmail.com>
2024-10-23 21:42:11 -0300

    Started working on order aggregate

--- OrderService/src/main/java/com/order/service/command/Order.java
+++ OrderService/src/main/java/com/order/service/command/Order.java
@@ -1,11 +1,17 @@
 package com.order.service.command;
 
+import com.order.service.coreapi.commands.CreateOrderCommand;
+import com.order.service.coreapi.queries.product.FindProductsQuery;
 import com.order.service.query.product.ProductView;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.queryhandling.QueryGateway;
 import org.axonframework.spring.stereotype.Aggregate;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
 @Aggregate
 public class Order {
@@ -15,4 +21,23 @@ public class Order {
     private List<ProductView> products;
     private long paymentMethod;
     private long totalPrice;
+
+    protected Order() {}
+
+    @CommandHandler
+    public Order(CreateOrderCommand command, QueryGateway queryGateway) {
+
+        try {
+            List<ProductView> productsView = queryGateway.query(
+                    new FindProductsQuery(command.getProducts()),
+                    ResponseTypes.multipleInstancesOf(ProductView.class)
+            ).get();
+
+
+
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
