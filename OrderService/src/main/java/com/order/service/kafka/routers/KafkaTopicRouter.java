dcaa7bf73d2f9f38436ceafe0cc56e46ed686005
Murilo358 <murilobarbosa358@gmail.com>
2024-10-30 20:39:38 -0300

    Mapped topics on KafkaTopicRouter

--- OrderService/src/main/java/com/order/service/kafka/routers/KafkaTopicRouter.java
+++ OrderService/src/main/java/com/order/service/kafka/routers/KafkaTopicRouter.java
@@ -1,34 +1,41 @@
 package com.order.service.kafka.routers;
 
+import com.order.service.coreapi.events.category.CategoryCreatedEvent;
+import com.order.service.coreapi.events.category.CategoryDeletedEvent;
+import com.order.service.coreapi.events.category.CategoryUpdatedEvent;
+import com.order.service.coreapi.events.order.OrderCreatedEvent;
+import com.order.service.coreapi.events.product.ProductCreatedEvent;
+import com.order.service.coreapi.events.product.ProductDeletedEvent;
+import com.order.service.coreapi.events.product.ProductInventoryUpdatedEvent;
+import com.order.service.coreapi.events.product.ProductUpdatedEvent;
+import com.order.service.coreapi.events.promotion.PromotionCreatedEvent;
+import com.order.service.coreapi.events.promotion.PromotionDeleteEvent;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class KafkaTopicRouter {
 
-    //TODO MAKE THIS USE EventProcessorType
-    private final List<String> topicEventList = new ArrayList<>();
+    private final Map<Class<?>, String> eventTopicMap = new HashMap<>();
 
     public KafkaTopicRouter() {
-        topicEventList.add("productCreated");
-        topicEventList.add("productDeleted");
-        topicEventList.add("productCreated");
-        topicEventList.add("productDeleted");
-        topicEventList.add("productInventoryUpdated");
-        topicEventList.add("productUpdated");
-        topicEventList.add("categoryCreated");
-        topicEventList.add("orderCreated");
-        topicEventList.add("categoryDeleted");
-        topicEventList.add("categoryUpdated");
-        topicEventList.add("promotionCreated");
-        topicEventList.add("promotionDeleted");
+
+        eventTopicMap.put(ProductCreatedEvent.class, "productCreated");
+        eventTopicMap.put(ProductDeletedEvent.class, "productDeleted");
+        eventTopicMap.put(ProductInventoryUpdatedEvent.class, "productInventoryUpdated");
+        eventTopicMap.put(ProductUpdatedEvent.class, "productUpdated");
+        eventTopicMap.put(CategoryCreatedEvent.class, "categoryCreated");
+        eventTopicMap.put(CategoryDeletedEvent.class, "categoryDeleted");
+        eventTopicMap.put(CategoryUpdatedEvent.class, "categoryUpdated");
+        eventTopicMap.put(PromotionCreatedEvent.class, "promotionCreated");
+        eventTopicMap.put(PromotionDeleteEvent.class, "promotionDeleted");
+        eventTopicMap.put(OrderCreatedEvent.class, "orderCreated");
 
     }
 
-
-    public List<String> getAllTopics() {
-        return topicEventList;
+    public String getTopicForEvent(Object event) {
+        return eventTopicMap.getOrDefault(event.getClass(), "DLQ");
     }
 }
