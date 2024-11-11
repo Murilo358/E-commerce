f55f43b889b0d96e6f53f060889cc7d21989408f
Murilo358 <murilobarbosa358@gmail.com>
2024-11-11 20:04:15 -0300

    Removed unused import

--- OrderService/src/main/java/com/order/service/query/order/OrderProjector.java
+++ OrderService/src/main/java/com/order/service/query/order/OrderProjector.java
@@ -18,7 +18,6 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @ProcessingGroup("events")
 @Component
