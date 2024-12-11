9c229fcfda3e7b6a11655a84f37ab320b5f4487b
Murilo358 <murilobarbosa358@gmail.com>
2024-12-11 20:37:00 -0300

    Removed @Generated value from PaymentMethodView

--- OrderService/src/main/java/com/order/service/query/paymentMethod/PaymentMethodView.java
+++ OrderService/src/main/java/com/order/service/query/paymentMethod/PaymentMethodView.java
@@ -1,9 +1,6 @@
 package com.order.service.query.paymentMethod;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +16,7 @@ import java.util.UUID;
 @Table(name="payment_methods", schema="users_management")
 public class PaymentMethodView {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
 
     @Column
