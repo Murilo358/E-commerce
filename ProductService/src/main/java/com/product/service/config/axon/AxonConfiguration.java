92bbd1140b7d4d3d3820b657e4c8c5ad9ccbfeb8
Murilo358 <murilobarbosa358@gmail.com>
2024-12-14 12:54:43 -0300

    Created global axon error handling

--- ProductService/src/main/java/com/product/service/config/axon/AxonConfiguration.java
+++ ProductService/src/main/java/com/product/service/config/axon/AxonConfiguration.java
@@ -4,12 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.product.service.config.jackson.JacksonAvroModule;
 import com.product.service.config.jackson.JacksonIgnoreAvroPropertiesMixIn;
+import lombok.extern.slf4j.Slf4j;
+import org.axonframework.config.ConfigurerModule;
+import org.axonframework.config.EventProcessingConfigurer;
+import org.axonframework.eventhandling.EventMessage;
+import org.axonframework.eventhandling.EventMessageHandler;
+import org.axonframework.eventhandling.ListenerInvocationErrorHandler;
+import org.axonframework.eventhandling.PropagatingErrorHandler;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import javax.annotation.Nonnull;
+
+@Slf4j
 @Configuration
 public class AxonConfiguration {
 
@@ -25,4 +35,24 @@ public class AxonConfiguration {
     }
 
 
+    @Bean
+    public ConfigurerModule processingGroupErrorHandlingConfigurerModule() {
+        return configurer -> configurer.eventProcessing(processingConfigurer ->
+                processingConfigurer.registerDefaultListenerInvocationErrorHandler(
+                                conf -> PropagatingErrorHandler.INSTANCE
+                        )
+                        .registerListenerInvocationErrorHandler(
+                                "my-processing-group",
+                                conf -> new ListenerInvocationErrorHandler() {
+                                    @Override
+                                    public void onError(@Nonnull Exception exception, @Nonnull EventMessage<?> event, @Nonnull EventMessageHandler eventHandler) throws Exception {
+                                        log.error("Error while processing event: {}", event, exception);
+                                    }
+
+                                }
+                        )
+        );
+    }
 }
+
+
