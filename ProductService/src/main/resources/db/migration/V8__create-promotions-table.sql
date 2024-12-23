2be90c86e1b737ac0155927b73c7ab1923c08d52
Murilo358 <murilobarbosa358@gmail.com>
2024-12-23 18:23:05 -0300

    Added new price into promotions table

--- ProductService/src/main/resources/db/migration/V8__create-promotions-table.sql
+++ ProductService/src/main/resources/db/migration/V8__create-promotions-table.sql
@@ -1,12 +1,16 @@
-CREATE TABLE catalog.promotions(
-    id BIGSERIAL  NOT NULL,
-    product_id UUID NOT NULL,
-    description TEXT NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL
+CREATE TABLE catalog.promotions
+(
+    id          BIGSERIAL NOT NULL,
+    product_id  UUID      NOT NULL,
+    new_price   BIGINT    NOT NULL,
+    description TEXT      NOT NULL,
+    start_date  DATE      NOT NULL,
+    end_date    DATE      NOT NULL
 );
 ALTER TABLE
-    catalog.promotions ADD PRIMARY KEY(id);
+    catalog.promotions
+    ADD PRIMARY KEY (id);
 
 ALTER TABLE
-    catalog.promotions ADD CONSTRAINT promotions_product_id_foreign FOREIGN KEY(product_id) REFERENCES catalog.products(id);
+    catalog.promotions
+    ADD CONSTRAINT promotions_product_id_foreign FOREIGN KEY (product_id) REFERENCES catalog.products (id);
