1ce709ca0138fb5257925a8db74664b8513a126b
Murilo358 <murilobarbosa358@gmail.com>
2024-12-30 12:54:16 -0300

    Created UserModel

---
+++ ProductService/src/main/java/com/product/service/enums/UserRole.java
@@ -0,0 +1,16 @@
+package com.product.service.enums;
+
+public enum UserRole {
+    SELLER("seller"),
+    BUYER("buyer");
+
+    private String role;
+
+    UserRole(String role){
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}
