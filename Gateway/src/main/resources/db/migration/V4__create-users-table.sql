3abe0780fa94196ce10ea3836cd231d1cb427e73
Murilo358 <murilobarbosa358@gmail.com>
2024-12-30 12:53:44 -0300

    Added is_enabled column to user's table

--- Gateway/src/main/resources/db/migration/V4__create-users-table.sql
+++ Gateway/src/main/resources/db/migration/V4__create-users-table.sql
@@ -1,15 +1,17 @@
-CREATE TABLE IF NOT EXISTS users_management.users(
-    id BIGSERIAL  NOT NULL,
-    name TEXT NOT NULL,
-    cpf TEXT NOT NULL,
-    birth_date DATE NOT NULL,
-    email TEXT NULL,
-    password TEXT NOT NULL,
-    role INTEGER NOT NULL,
-    profile_id BIGINT NOT NULL
-);
-ALTER TABLE
-    users_management.users ADD PRIMARY KEY(id);
-
-ALTER TABLE
-    users_management.users ADD CONSTRAINT users_id_foreign FOREIGN KEY(profile_id) REFERENCES users_management.profile(id);
+create table
+    users_management.users
+(
+    id         bigserial,
+    birth_date date   not null,
+    cpf        text   not null,
+    email      text   not null,
+    name       text   not null,
+    password   text   not null,
+    role       text null,
+    created_at timestamp without time zone null,
+    constraint users_pkey primary key (id),
+    constraint users_cpf_key unique (cpf),
+    constraint users_email_key unique (email),
+    profile_id BIGINT NOT NULL,
+    is_enabled BOOLEAN DEFAULT(FALSE)
+) tablespace pg_default;
